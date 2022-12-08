bd022ae3348e27975a0452ce9355cce206332d07
DemiZ <21-45094-2@student.aiub.edu>
2022-12-08 13:42:26 +0600

    First button added

--- Project-Kothin/Form1.cs
+++ Project-Kothin/Form1.cs
@@ -16,5 +16,10 @@ namespace Project_Kothin
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
