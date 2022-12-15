4c7a00faebed19d413735a9a2a8cb7c172845d8a
Arif Absar <arifabsar182@gmail.com>
2022-12-15 18:35:25 +0600

    service gone

--- Project-Kothin/Program.cs
+++ Project-Kothin/Program.cs
@@ -16,7 +16,7 @@ namespace Project_Kothin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Service());
+            
         }
     }
 }
