a535310bd0e847ff5242c49285a9e6c8239a55e0
Albar Hossain <albarhossain@gmail.com>
2022-12-16 21:56:00 +0600

    idk

---
+++ Project-Kothin/PrintPDF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Kothin
+{
+    public partial class PrintPDF : Form
+    {
+        public PrintPDF()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPrintPdf_Click(object sender, EventArgs e)
+        {
+            Document document =
+        }
+    }
+}
